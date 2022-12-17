1db6f1d91917c94ee6eb337938f103073cf79340
aly <alyssonrosa1@gmail.com>
2022-12-16 21:30:43 -0300

    party mode hehe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import { useLayoutEffect, useState } from 'react';
-import { Shuffle } from 'phosphor-react';
+import { Shuffle, Equalizer } from 'phosphor-react';
 import styles from '../styles/Home.module.scss';
 
 import { Text } from '../components/Text/Text';
 import { Button } from '../components/Button/Button';
 import { NavTabs } from '../components/NavTabs/NavTabs';
+import { clearTimeout } from 'timers';
 
 export default function Home() {
   const [hex, setHex] = useState<string>('');
+  const [party, setParty] = useState<boolean>(false);
 
   const generateColor = () => {
     const digits: string = '0123456789ABCDEF';
@@ -28,10 +30,24 @@ export default function Home() {
     document.getElementById("color").style.background = hex;
   }, [hex]);
 
+  useLayoutEffect(() => {
+    if (party) {
+      const interval = setInterval(() => {
+        generateColor();
+      }, 100);
+
+      return () => clearInterval(interval);
+    }
+  }, [party]);
+
   return (
     <div id="color" className={styles.wrapper}>
 
       <NavTabs>
+        <Button onClick={() => setParty(!party)}>
+          <Equalizer size={24} />
+        </Button>
+
         <Button onClick={generateColor}>
           <Shuffle size={24} />
         </Button>
